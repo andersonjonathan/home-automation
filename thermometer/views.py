8c865f654ff3fa344ef09cc35196abc80f974101
jonathan <jonathan@jonathananderson.se>
2016-07-31 20:39:42 +0200

    Added link for thermometers

--- thermometer/views.py
+++ thermometer/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def thermometers(request):
-    return render(request, 'thermometer/thermometers.html')
+    return render(request, 'thermometer/thermometers.html', {'current_page': "Thermometers"})
