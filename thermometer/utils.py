74214976b30880ef0f3a1e41820278a3d6a996aa
jonathan <jonathan@jonathananderson.se>
2016-08-02 23:55:49 +0200

    raw value capacitor

--- thermometer/utils.py
+++ thermometer/utils.py
@@ -17,6 +17,28 @@ def read_retry(pin):
     return Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, pin)
 
 
+def _RCtime_no_sleep(RCpin):
+    GPIO.setmode(GPIO.BCM)
+    reading = 0
+    GPIO.setup(RCpin, GPIO.OUT)
+    GPIO.output(RCpin, GPIO.LOW)
+    time.sleep(0.1)
+    GPIO.setup(RCpin, GPIO.IN)
+    while GPIO.input(RCpin) == GPIO.LOW:
+        reading += 1
+    return reading
+
+
+def read_capacitor_raw(pin):
+    temps = []
+    i = 0
+    while True:
+        temps.append(_RCtime(pin))
+        i += 1
+        if i >= 19:
+            return max(temps)
+
+
 def _RCtime(RCpin):
     GPIO.setmode(GPIO.BCM)
     reading = 0
